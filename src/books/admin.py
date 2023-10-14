0688d4e2ebc4015be6158434ddfcc54c19337cf1
Chayka1 <m.chayka.v@gmail.com>
2023-10-14 17:03:01 +0300

    🎉 Create new admin panels

--- src/books/admin.py
+++ src/books/admin.py
@@ -1,1 +1,36 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from books.models import Book, ReadingSession
+
+
+@admin.register(Book)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "author",
+        "publication_year",
+        "short_description",
+        "full_description",
+    ]
+    list_filter = ["author"]
+    search_fields = [
+        "title",
+        "author",
+        "publication_year",
+    ]
+
+
+@admin.register(ReadingSession)
+class UserAdmin(admin.ModelAdmin):  # noqa
+    readonly_fields = ["user", "book", "start_time", "end_time"]
+    list_display = [
+        "user",
+        "book",
+        "start_time",
+        "end_time",
+    ]
+    list_filter = ["user", "book"]
+    search_fields = [
+        "user",
+        "book",
+    ]
--- src/users/admin.py
+++ src/users/admin.py
@@ -1,1 +1,28 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    exclude = ["groups", "password"]
+    readonly_fields = [
+        "last_login",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "email",
+        "total_reading_time_7_days",
+        "total_reading_time_30_days",
+    ]
+    list_filter = ["is_active", "first_name"]
+    list_display = [
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "total_reading_time_7_days",
+        "total_reading_time_30_days",
+    ]
+    search_fields = ["email", "first_name"]
